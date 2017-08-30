9beaf50a2ddd4f6f559c4df4789f398bbc5339cd
George Spanos <gespa11019@gmail.com>
2017-08-31 00:14:31 +0300

    test 11

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const auctionSchema = new Schema ({
 })
 const Bid = mongoose.model('Bid',auctionSchema);
 app.post('/', function (req, res ,next) {
-     console.log(req.body);
+     console.log(req);
   var bid = Bid({
       array: req.body.hand,
       hcp: req.body.hcp,
