f4199b16c3b93cc71fb15cf3e343fa3b60a3d32f
George Spanos <gespa11019@gmail.com>
2017-08-31 00:16:43 +0300

    test 11

--- src/app/bridge.service.ts
+++ src/app/bridge.service.ts
@@ -81,6 +81,7 @@ export class BridgeApi {
     toDatabase() {
       const header = new Headers({'Content-Type': 'application/json'});
       const body = JSON.stringify(this.bid);
+      console.log(body);
       return this.http.post('/', body, {headers: header})
       .map(
         ( response: Response) => { response.json(); }
