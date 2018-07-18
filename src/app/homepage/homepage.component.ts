e161c7ae7c2615542d17f90ef27ea1bffc3068af
George-Spanos <gespa11019@gmail.com>
2018-07-18 20:59:18 +0300

    Revert "no hello"
    This reverts commit 84c999cb278e0bff55eb8e3a1d558adb03f8d279.

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { routeFadeStateTrigger } from '../shared/animations/animations';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css'],
+  animations: [
+    routeFadeStateTrigger
+  ]
+})
+export class HomepageComponent implements OnInit {
+  @HostBinding('@routeFadeState') routeAnimation = true;
+  constructor() { }
+
+  ngOnInit() {
+    console.log('hello');
+  }
+
+}
